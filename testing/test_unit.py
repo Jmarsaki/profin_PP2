7606b78fe823db0ba41c357a4f2b547a066ab809
Julio Mariano Sachi <74797751+Jmarsaki@users.noreply.github.com>
2023-12-23 16:22:07 -0300

    Delete testing directory

--- testing/test_unit.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def test_unit():
-    app = Flask(__name__)  # Crear una instancia de la aplicación Flask
-    assert app is not None, "Error: Flask app is not initialized"  # Verificar que la aplicación no es None
-    print("listo")  # Imprimir "listo" si la prueba es exitosa
-
-# Prueba de la función test_unit
-test_unit()
